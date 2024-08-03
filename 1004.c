aee3fc98cbad1cd1ce3180d83d73d0f0267fa87a
MFGROOVE <154380754+MFGROOVE@users.noreply.github.com>
2024-08-03 00:33:53 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ int a;
+ int b;
+ int PROD;
+ scanf("%d %d",&a,&b);
+  PROD=a*b ;
+ printf("PROD = %d\n",PROD);
+    return 0;
+}
