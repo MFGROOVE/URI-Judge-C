58cdd2790cc41e12f13aec771d1febc6221679fe
MFGROOVE <154380754+MFGROOVE@users.noreply.github.com>
2024-08-04 16:04:52 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ float a,b;
+ scanf("%f %f",&a,&b);
+ float MEDIA=(a*3.5+b*7.5)/11;
+ printf("MEDIA = %.5f\n",MEDIA);
+    return 0;
+}
